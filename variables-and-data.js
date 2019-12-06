1e78cee460172c8201a24aa7a28d79eea278ca1f
Reece Nunn <rmnunn5@gmail.com>
2019-12-06 11:12:04 -0700

    Learned about variables and data types

---
+++ variables-and-data.js
@@ -0,0 +1,106 @@
+/*
+ * Variables, Data Types, and Typing
+ */
+
+/*
+//Equals
+let equals = 1 === 1;
+console.log(equals);
+
+// Greater than
+let greaterThan = 5 > 1;
+
+//Less Than
+let lessThan = 2 < 10;
+
+//Greater than or equal To
+let greaterThanEq = 5 >= 5;
+
+//Less than equals
+let lessThanEq = 4 <= 9;
+
+// Not Equals
+let notEquals = 5 !== 2;
+
+
+let storeA = 2.40;
+let storeB = 3.40;
+
+let storeAisLower = storeA < storeB;
+console.log(storeAisLower);
+
+function compareStorePrices (storeA, storeB) {
+	let storeAisLower = storeA < storeB;
+	if (storeAisLower) {
+		console.log("Store A has a lower price")
+	} else if (storeB < storeA) {
+		console.log("Store B has the lower price")
+	} else {
+		console.log("Their prices are equal")
+	}
+}
+//compareStorePrices(10, 5);
+//compareStorePrices(13, 16);
+
+function squarenum (number) {
+	let squared = number * number;
+	return squared;
+}
+
+let squaredNumber = squarenum(10);
+console.log(squaredNumber);
+
+ */
+/*
+let x = 10;
+
+function addNumbers (n, m, x) {
+	console.log(x);
+	let b = 8;
+	if (1===1) {
+		b = 8;
+	}
+	console.log(b);
+	return n + m;
+}
+
+addNumbers(2, 6, 8)
+console.log(x);
+
+ */
+//              0  1  2  3  4  5  6
+
+/*
+let ourArray = [1, 2, 3, 4, 5, 6, 7];
+//console.log(ourArray[4][0]);
+let arrLen = ourArray.length;
+for(let i = 0; i <= arrLen; i ++){
+	console.log("i is equal to: " + i);
+	console.log(ourArray[i]);
+	for(let j = 0; j <= 10; j ++) {
+		console.log('j is equal to: ' + j);
+	}
+}
+
+ */
+
+let x = 0
+while (x <= 10) {
+	console.log('Ran');
+	x++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
